01538a57fd5d7aaca0fb7e855169ec41b8076f82
Amir tarek <amir.tarek8880@gmail.com>
2023-12-14 21:26:40 +0200

    add arrows to show all products in checkout page or show one product

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
-import React, { useState } from 'react';
-import { StatusBar, Text } from 'react-native';
-import { COLORS } from './src/constants';
+import React, {useState} from 'react';
+import {StatusBar, Text} from 'react-native';
+import {COLORS} from './src/constants';
 import Cart from './src/screens/Home/Cart';
 import Home_Page from './src/screens/Home/Home_Page';
 import CheckoutPage from './src/screens/Home/CheckoutPage';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import AnimTab2 from './src/screens/Bottomtabs/AnimTab2';
 import StartScreen from './src/screens/Splash/StartScreen';
 
 const App = () => {
-
-
-
   return (
     <>
       <StatusBar animated backgroundColor={COLORS.TrybackgroundColorIce1} />
@@ -20,7 +17,7 @@ const App = () => {
       {/* <NavigationContainer>
       <AnimTab2/>
       </NavigationContainer> */}
-      <CheckoutPage/>
+      <CheckoutPage />
     </>
   );
 };
